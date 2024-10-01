837f04128f79e0aa60493910e6cb2ff19b6466f1
Sabin Bhattarai <95851999+codenothappening@users.noreply.github.com>
2024-10-01 19:59:41 +0545

    time function added

--- lib/common/custom_appbar.dart
+++ lib/common/custom_appbar.dart
@@ -18,41 +18,60 @@ class CustomAppbar extends StatelessWidget {
         margin: EdgeInsets.only(top: 20.h),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            CircleAvatar(
-              radius: 25.r,
-              backgroundColor: kSecondary,
-              backgroundImage: const NetworkImage(
-                  "https://img.freepik.com/free-vector/user-blue-gradient_78370-4692.jpg?w=996&t=st=1727787261~exp=1727787861~hmac=38ceae8896ad38121ff9176721a9058ef0eb89a86cb9eeae26f8c6db8a2b5f1b"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 6.h, left: 8.w),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ReusableTextWidget(
-                    text: "Deliver to",
-                    style: appStyle(13, kSecondary, FontWeight.w600),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                CircleAvatar(
+                  radius: 23.r,
+                  backgroundColor: kSecondary,
+                  backgroundImage: const NetworkImage(
+                      "https://img.freepik.com/free-vector/user-blue-gradient_78370-4692.jpg?w=996&t=st=1727787261~exp=1727787861~hmac=38ceae8896ad38121ff9176721a9058ef0eb89a86cb9eeae26f8c6db8a2b5f1b"),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 6.h, left: 8.w),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ReusableTextWidget(
+                        text: "Deliver to",
+                        style: appStyle(13, kSecondary, FontWeight.w600),
+                      ),
+                      SizedBox(
+                        width: width * 0.65,
+                        child: Text(
+                          "16768 21st Ave N, Plymouth, MN 55447",
+                          overflow: TextOverflow.ellipsis,
+                          style: appStyle(11, kGrayLight, FontWeight.normal),
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    width: width * 0.65,
-                    child: Text(
-                      "16768 21st Ave N, Plymouth, MN 55447",
-                      overflow: TextOverflow.ellipsis,
-                      style: appStyle(11, kGrayLight, FontWeight.normal),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const Text(
-              "☀️",
-              style: TextStyle(fontSize: 35),
+            Text(
+              getTimeOfDay(),
+              style: const TextStyle(fontSize: 35),
             )
           ],
         ),
       ),
     );
+  }
+
+  String getTimeOfDay() {
+    DateTime now = DateTime.now();
+    int hour = now.hour;
+    if (hour >= 0 && hour < 12) {
+      return " ☀️ ";
+    } else if (hour >= 12 && hour < 16) {
+      return " ⛅ ";
+    } else {
+      return " 🌙 ";
+    }
   }
 }
