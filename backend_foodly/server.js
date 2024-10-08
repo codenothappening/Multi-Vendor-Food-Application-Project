fb8114842fc18fa93962bc6a1370952700ff383f
Sabin Bhattarai <95851999+codenothappening@users.noreply.github.com>
2024-10-08 10:27:30 +0545

    changes

--- backend_foodly/server.js
+++ backend_foodly/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const dotenv = require('dotenv')
 
 app.get('/', (req, res) => res.send("Hello World"))
 app.listen(port, () => console.log(`Foodly Backend is Running on ${port}!`))
