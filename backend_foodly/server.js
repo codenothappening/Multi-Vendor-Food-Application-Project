d64e4c2d8334dbd2f7458fc1d688bfb5c8a4e4da
Sabin Bhattarai <95851999+codenothappening@users.noreply.github.com>
2024-10-08 15:16:46 +0545

    Category backend

--- backend_foodly/server.js
+++ backend_foodly/server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 const dotenv = require('dotenv');
 const mangoose = require('mongoose');
+const CategoryRoute = require("./routes/category")
 
 
 dotenv.config();
@@ -10,5 +11,10 @@ mangoose.connect(process.env.MONGOURL)
     .then(() => console.log("Foodly connected to Database"))
     .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send("Hello World"))
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+app.use("/api/category",CategoryRoute);
+
 app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is Running on ${process.env.PORT}!`))
